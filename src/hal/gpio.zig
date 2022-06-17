846508956eefbc82a7fb11390264c28716e11ea6
Andres Navarro <canavarro82@gmail.com>
2022-06-17 17:26:03 -0300

    Changed function to switch statements
    The GPIOCtrl function was throwing an error on reg:anytype

--- src/hal/gpio.zig
+++ src/hal/gpio.zig
@@ -52,58 +52,39 @@ pub const GPIO = enum {
     P29,
 };
 
-fn GPIOCtrl(comptime CtrlReg: type) type {
-    _ = CtrlReg;
-
-    return struct {
-        reg: anytype,
-
-        const Self = @This();
-
-        fn init(ctrl_reg: anytype) Self {
-            return Self{ .reg = ctrl_reg };
-        }
-
-        pub fn get_ctrl_reg_for(pin: GPIO) Self {
-            return switch (pin) {
-                GPIO.P0 => init(regs.IO_BANK0.GPIO1_CTRL),
-                GPIO.P1 => init(regs.IO_BANK0.GPIO1_CTRL),
-                GPIO.P2 => init(regs.IO_BANK0.GPIO2_CTRL),
-                GPIO.P3 => init(regs.IO_BANK0.GPIO3_CTRL),
-                GPIO.P4 => init(regs.IO_BANK0.GPIO4_CTRL),
-                GPIO.P5 => init(regs.IO_BANK0.GPIO5_CTRL),
-                GPIO.P6 => init(regs.IO_BANK0.GPIO6_CTRL),
-                GPIO.P7 => init(regs.IO_BANK0.GPIO7_CTRL),
-                GPIO.P8 => init(regs.IO_BANK0.GPIO8_CTRL),
-                GPIO.P9 => init(regs.IO_BANK0.GPIO9_CTRL),
-                GPIO.P10 => init(regs.IO_BANK0.GPIO10_CTRL),
-                GPIO.P11 => init(regs.IO_BANK0.GPIO11_CTRL),
-                GPIO.P12 => init(regs.IO_BANK0.GPIO12_CTRL),
-                GPIO.P13 => init(regs.IO_BANK0.GPIO13_CTRL),
-                GPIO.P14 => init(regs.IO_BANK0.GPIO14_CTRL),
-                GPIO.P15 => init(regs.IO_BANK0.GPIO15_CTRL),
-                GPIO.P16 => init(regs.IO_BANK0.GPIO16_CTRL),
-                GPIO.P17 => init(regs.IO_BANK0.GPIO17_CTRL),
-                GPIO.P18 => init(regs.IO_BANK0.GPIO18_CTRL),
-                GPIO.P19 => init(regs.IO_BANK0.GPIO19_CTRL),
-                GPIO.P20 => init(regs.IO_BANK0.GPIO20_CTRL),
-                GPIO.P21 => init(regs.IO_BANK0.GPIO21_CTRL),
-                GPIO.P22 => init(regs.IO_BANK0.GPIO22_CTRL),
-                GPIO.P23 => init(regs.IO_BANK0.GPIO23_CTRL),
-                GPIO.P24 => init(regs.IO_BANK0.GPIO24_CTRL),
-                GPIO.P25 => init(regs.IO_BANK0.GPIO25_CTRL),
-                GPIO.P26 => init(regs.IO_BANK0.GPIO26_CTRL),
-                GPIO.P27 => init(regs.IO_BANK0.GPIO27_CTRL),
-                GPIO.P28 => init(regs.IO_BANK0.GPIO28_CTRL),
-                GPIO.P29 => init(regs.IO_BANK0.GPIO29_CTRL),
-            };
-        }
-    };
-}
-
 pub fn set_dir(comptime pin: GPIO, dir: IODir) void {
-    const gpio_ctrl = GPIOCtrl(@TypeOf(pin)).get_ctrl_reg_for(pin);
-    const ctrl_reg = gpio_ctrl.reg;
+    const ctrl_reg = switch (pin) {
+        GPIO.P0 => regs.IO_BANK0.GPIO1_CTRL,
+        GPIO.P1 => regs.IO_BANK0.GPIO1_CTRL,
+        GPIO.P2 => regs.IO_BANK0.GPIO2_CTRL,
+        GPIO.P3 => regs.IO_BANK0.GPIO3_CTRL,
+        GPIO.P4 => regs.IO_BANK0.GPIO4_CTRL,
+        GPIO.P5 => regs.IO_BANK0.GPIO5_CTRL,
+        GPIO.P6 => regs.IO_BANK0.GPIO6_CTRL,
+        GPIO.P7 => regs.IO_BANK0.GPIO7_CTRL,
+        GPIO.P8 => regs.IO_BANK0.GPIO8_CTRL,
+        GPIO.P9 => regs.IO_BANK0.GPIO9_CTRL,
+        GPIO.P10 => regs.IO_BANK0.GPIO10_CTRL,
+        GPIO.P11 => regs.IO_BANK0.GPIO11_CTRL,
+        GPIO.P12 => regs.IO_BANK0.GPIO12_CTRL,
+        GPIO.P13 => regs.IO_BANK0.GPIO13_CTRL,
+        GPIO.P14 => regs.IO_BANK0.GPIO14_CTRL,
+        GPIO.P15 => regs.IO_BANK0.GPIO15_CTRL,
+        GPIO.P16 => regs.IO_BANK0.GPIO16_CTRL,
+        GPIO.P17 => regs.IO_BANK0.GPIO17_CTRL,
+        GPIO.P18 => regs.IO_BANK0.GPIO18_CTRL,
+        GPIO.P19 => regs.IO_BANK0.GPIO19_CTRL,
+        GPIO.P20 => regs.IO_BANK0.GPIO20_CTRL,
+        GPIO.P21 => regs.IO_BANK0.GPIO21_CTRL,
+        GPIO.P22 => regs.IO_BANK0.GPIO22_CTRL,
+        GPIO.P23 => regs.IO_BANK0.GPIO23_CTRL,
+        GPIO.P24 => regs.IO_BANK0.GPIO24_CTRL,
+        GPIO.P25 => regs.IO_BANK0.GPIO25_CTRL,
+        GPIO.P26 => regs.IO_BANK0.GPIO26_CTRL,
+        GPIO.P27 => regs.IO_BANK0.GPIO27_CTRL,
+        GPIO.P28 => regs.IO_BANK0.GPIO28_CTRL,
+        GPIO.P29 => regs.IO_BANK0.GPIO29_CTRL,
+    };
     switch (dir) {
         IODir.Input => ctrl_reg.modify(.{ .OEOVER = 0x02 }),
         IODir.Output => ctrl_reg.modify(.{ .OEOVER = 0x03 }),
@@ -111,8 +92,39 @@ pub fn set_dir(comptime pin: GPIO, dir: IODir) void {
 }
 
 pub fn set_function(comptime pin: GPIO, function: Function) void {
-    const gpio_ctrl = GPIOCtrl(@TypeOf(pin)).get_ctrl_reg_for(pin);
-    const ctrl_reg = gpio_ctrl.reg;
+    const ctrl_reg = switch (pin) {
+        GPIO.P0 => regs.IO_BANK0.GPIO1_CTRL,
+        GPIO.P1 => regs.IO_BANK0.GPIO1_CTRL,
+        GPIO.P2 => regs.IO_BANK0.GPIO2_CTRL,
+        GPIO.P3 => regs.IO_BANK0.GPIO3_CTRL,
+        GPIO.P4 => regs.IO_BANK0.GPIO4_CTRL,
+        GPIO.P5 => regs.IO_BANK0.GPIO5_CTRL,
+        GPIO.P6 => regs.IO_BANK0.GPIO6_CTRL,
+        GPIO.P7 => regs.IO_BANK0.GPIO7_CTRL,
+        GPIO.P8 => regs.IO_BANK0.GPIO8_CTRL,
+        GPIO.P9 => regs.IO_BANK0.GPIO9_CTRL,
+        GPIO.P10 => regs.IO_BANK0.GPIO10_CTRL,
+        GPIO.P11 => regs.IO_BANK0.GPIO11_CTRL,
+        GPIO.P12 => regs.IO_BANK0.GPIO12_CTRL,
+        GPIO.P13 => regs.IO_BANK0.GPIO13_CTRL,
+        GPIO.P14 => regs.IO_BANK0.GPIO14_CTRL,
+        GPIO.P15 => regs.IO_BANK0.GPIO15_CTRL,
+        GPIO.P16 => regs.IO_BANK0.GPIO16_CTRL,
+        GPIO.P17 => regs.IO_BANK0.GPIO17_CTRL,
+        GPIO.P18 => regs.IO_BANK0.GPIO18_CTRL,
+        GPIO.P19 => regs.IO_BANK0.GPIO19_CTRL,
+        GPIO.P20 => regs.IO_BANK0.GPIO20_CTRL,
+        GPIO.P21 => regs.IO_BANK0.GPIO21_CTRL,
+        GPIO.P22 => regs.IO_BANK0.GPIO22_CTRL,
+        GPIO.P23 => regs.IO_BANK0.GPIO23_CTRL,
+        GPIO.P24 => regs.IO_BANK0.GPIO24_CTRL,
+        GPIO.P25 => regs.IO_BANK0.GPIO25_CTRL,
+        GPIO.P26 => regs.IO_BANK0.GPIO26_CTRL,
+        GPIO.P27 => regs.IO_BANK0.GPIO27_CTRL,
+        GPIO.P28 => regs.IO_BANK0.GPIO28_CTRL,
+        GPIO.P29 => regs.IO_BANK0.GPIO29_CTRL,
+    };
+
     switch (function) {
         Function.F0 => ctrl_reg.modify(.{ .FUNCSEL = 0x00 }),
         Function.F1 => ctrl_reg.modify(.{ .FUNCSEL = 0x01 }),
