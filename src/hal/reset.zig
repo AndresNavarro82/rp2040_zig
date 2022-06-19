3053ada238042777c2322f4ad6f529d671672b98
Andres Navarro <canavarro82@gmail.com>
2022-06-19 00:27:25 -0300

    Added serial port initialization and TX test

--- src/hal/reset.zig
+++ src/hal/reset.zig
@@ -2,10 +2,18 @@ const regs = @import("rp2040_ras");
 
 const Peripheral = enum {
     IO_BANK0,
+    UART0,
 };
 
 pub fn deassert_reset_of(perif: Peripheral) void {
     switch (perif) {
-        .IO_BANK0 => regs.RESETS.RESET.modify(.{ .io_bank0 = 0 }),
+        .IO_BANK0 => {
+            regs.RESETS.RESET.modify(.{ .io_bank0 = 0 });
+            while ((regs.RESETS.RESET_DONE.read().io_bank0) == 0) {}
+        },
+        .UART0 => {
+            regs.RESETS.RESET.modify(.{ .uart0 = 0 });
+            while ((regs.RESETS.RESET_DONE.read().uart0) == 0) {}
+        },
     }
 }
--- src/main.zig
+++ src/main.zig
@@ -34,15 +34,56 @@ fn clocks_init() void {
     regs.CLOCKS.CLK_SYS_CTRL.modify(.{ .SRC = SYS_SRC_REF });
 
     // Configure peri clock to XOSC (12MHz)
-    const PERI_ENABLE = 0x1;
     const PERI_SRC_XOSC = 0x4;
-    regs.CLOCKS.CLK_PERI_CTRL.modify(.{ .ENABLE = PERI_ENABLE, .AUXSRC = PERI_SRC_XOSC });
+    regs.CLOCKS.CLK_PERI_CTRL.modify(.{ .ENABLE = 1, .AUXSRC = PERI_SRC_XOSC });
+}
+
+fn uart0_config() void {
+    // uart0 & io_bank0 should be running (reset deasserted)
+
+    // First set 115200 baud rate
+    // See RP2040 Datasheet:
+    // 4.2.3.2.1. Fractional baud rate divider
+    //(12000000/(16/115200)) = 6.514
+    //0.514*64 = 32.666
+    const BAUD_INT = 6;
+    const BAUD_FRAC = 33;
+    regs.UART0.UARTIBRD.modify(.{ .BAUD_DIVINT = BAUD_INT });
+    regs.UART0.UARTFBRD.modify(.{ .BAUD_DIVFRAC = BAUD_FRAC });
+
+    const LEN8 = 0x3;
+    // 8n1 (8 bits, no parity, 1 stop bit), enable FIFOs
+    regs.UART0.UARTLCR_H.modify(.{ .WLEN = LEN8, .FEN = 1 });
+    // enable uart, no hardware flow control, enable RX & TX
+    regs.UART0.UARTCR.modify(.{ .UARTEN = 1, .TXE = 1, .RXE = 1 });
+
+    // set gpio 0 function as TX & gpio 1 function as RX
+    // See RP2040 Datasheet:
+    // 2.19.2. Function Select
+    const UART_FN = gpio.Function.F2;
+    gpio.set_function(.P0, UART_FN);
+    gpio.set_function(.P1, UART_FN);
+}
+
+fn uart_send_char(char:u8) void {
+    // wait until uart is ready (FIFO not full)
+    while(regs.UART0.UARTFR.read().TXFF == 1) { }
+    regs.UART0.UARTDR.modify(.{ .DATA = char });
+}
+
+fn uart_send_string(str: [] const u8) void {
+    for (str) |char| {
+        uart_send_char(char);
+    }
 }
 
 pub fn hako_main() noreturn {
     clocks_init();
 
     reset.deassert_reset_of(.IO_BANK0);
+    reset.deassert_reset_of(.UART0);
+
+    uart0_config();
 
     gpio.set_dir(.P25, .Output);
     gpio.set_function(.P25, .F5);
@@ -57,6 +98,9 @@ pub fn hako_main() noreturn {
 
         sio.clear_output(.GPIO25);
         busy_wait();
+
+        // minicom doesn't like just \n without some additional config
+        uart_send_string("Hello RP2040!\r\n");
     }
 }
 
